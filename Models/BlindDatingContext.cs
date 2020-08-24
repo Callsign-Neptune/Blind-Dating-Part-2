13e454cb84f9b56b5be1c6ee7b7d98ed01f423da
Callsign-Neptune <66689267+Callsign-Neptune@users.noreply.github.com>
2020-08-24 12:26:53 -0500

    Add files via upload

---
+++ Models/BlindDatingContext.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace BlindDating.Models
+{
+    public partial class BlindDatingContext : DbContext
+    {
+        public BlindDatingContext()
+        {
+        }
+
+        public BlindDatingContext(DbContextOptions<BlindDatingContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<DatingProfile> DatingProfile { get; set; }
+        public virtual DbSet<MailMessages> MailMessages { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=localhost\\sqlexpress;Database=BlindDating;Trusted_Connection=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DatingProfile>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Age).HasColumnName("age");
+
+                entity.Property(e => e.Bio)
+                    .IsRequired()
+                    .HasColumnName("bio")
+                    .HasColumnType("text");
+
+                entity.Property(e => e.DisplayName)
+                    .HasColumnName("displayName")
+                    .HasMaxLength(40)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Firstname)
+                    .IsRequired()
+                    .HasColumnName("firstname")
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Gender)
+                    .IsRequired()
+                    .HasColumnName("gender")
+                    .HasMaxLength(1)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Lastname)
+                    .IsRequired()
+                    .HasColumnName("lastname")
+                    .HasMaxLength(30)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.PhotoPath)
+                    .HasColumnName("photoPath")
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UserAccountId)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<MailMessages>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.FromProfileId).HasColumnName("fromProfileID");
+
+                entity.Property(e => e.IsRead).HasColumnName("isRead");
+
+                entity.Property(e => e.MessageText)
+                    .IsRequired()
+                    .HasColumnName("messageText")
+                    .HasColumnType("text");
+
+                entity.Property(e => e.MessageTitle)
+                    .IsRequired()
+                    .HasColumnName("messageTitle")
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ToProfileId).HasColumnName("toProfileID");
+
+                entity.HasOne(d => d.FromProfile)
+                    .WithMany(p => p.MailMessagesFromProfile)
+                    .HasForeignKey(d => d.FromProfileId)
+                    .HasConstraintName("FK__MailMessa__fromP__628FA481");
+
+                entity.HasOne(d => d.ToProfile)
+                    .WithMany(p => p.MailMessagesToProfile)
+                    .HasForeignKey(d => d.ToProfileId)
+                    .HasConstraintName("FK__MailMessa__toPro__6383C8BA");
+            });
+        }
+    }
+}
---
+++ Models/DatingProfile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlindDating.Models
+{
+    public partial class DatingProfile
+    {
+        public DatingProfile()
+        {
+            MailMessagesFromProfile = new HashSet<MailMessages>();
+            MailMessagesToProfile = new HashSet<MailMessages>();
+        }
+
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string Bio { get; set; }
+        public string UserAccountId { get; set; }
+        public string DisplayName { get; set; }
+        public string PhotoPath { get; set; }
+
+        public ICollection<MailMessages> MailMessagesFromProfile { get; set; }
+        public ICollection<MailMessages> MailMessagesToProfile { get; set; }
+    }
+}
